09755ac54a711e5511919bb886b58ee2c0648c02
Luizedu10s <luiz-souza.ls@academico.ifpb.edu.br>
2023-12-16 13:33:00 -0300

    Aprendendo sobre a função render().

--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 # VIEW HELLO   
 def hello(request):
-    return HttpResponse('<h1>Hello App django in live.</h1>')
+    return render(request, 'temp/hello.html')
 
 # VIEW HOME
 def home(request):
-    return HttpResponse('<h1> Home page. </h1>')
+    return render(request, 'temp/home.html')
